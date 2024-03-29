adffc1eaf9ebf58b249004f4c4d2e004b4e59398
Swaraj Zende <zendeswaraj53@gmail.com>
2024-03-29 16:52:56 +0530

    Added a Label in window at specific location

--- tutorial1.py
+++ tutorial1.py
@@ -7,8 +7,12 @@ def window():
     win = QMainWindow()
 
     # Set Size and label of window
-    win.setGeometry(200,100,300,300) # xpos,ypos,width,height
+    win.setGeometry(200,100,300,300) # xpos,ypos,width,height (origin = Top left corner) 
     win.setWindowTitle("Swaraj_learning_PyQt5")
+
+    label = QtWidgets.QLabel(win)
+    label.setText("Hello World!")
+    label.move(50,50)
 
     win.show()
 
